ebd32138ba8b2682844651e89dbfd7b8e589e948
rohithnafeel <devarc16@gmail.com>
2026-07-08 12:44:45 +0530

    Made the Build profile

---
+++ bulid/Sandbox/CMakeFiles/Sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sandbox.dir/src/main.cpp.obj"
+  "CMakeFiles/Sandbox.dir/src/main.cpp.obj.d"
+  "Sandbox.exe"
+  "Sandbox.exe.manifest"
+  "Sandbox.pdb"
+  "libSandbox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
